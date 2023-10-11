4016aadd8eafcbe1da48f86a61b2168ba1fd110c
munirathna-8 <147590770+munirathna-8@users.noreply.github.com>
2023-10-11 12:41:51 +0530

    Add files via upload

---
+++ Challenge 3.1.py
@@ -0,0 +1,4 @@
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes"]
+target = "Apple"
+result = linear_search_product(products, target)
+print(result)  # This will print [0, 3], as "Apple" is found at indices 0 and 3 in the list.
